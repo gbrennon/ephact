60b54c2a7c7ed559102d2e5822cf86d0e1faa153
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-07 16:33:20 -0300

    feat(application): expose branding info application port

--- src/application/ports/mod.rs
+++ src/application/ports/mod.rs
@@ -5,4 +5,5 @@ pub use inbound::{
     list_actions_port::ListActionsPort, list_workflows_port::ListWorkflowsPort,
     run_action_port::RunActionPort, run_all_workflows_port::RunAllWorkflowsPort,
     run_workflow_port::RunWorkflowPort,
+    show_project_branding_info_port::ShowProjectBrandingInfoPort,
 };
