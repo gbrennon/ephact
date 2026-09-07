b70993da45d86fffcb95480574ee16e267f2e2a7
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-07 15:05:26 -0300

    feat(domain): register branding value objects in value_objects module

--- src/domain/value_objects/mod.rs
+++ src/domain/value_objects/mod.rs
@@ -7,6 +7,10 @@ pub mod action_reference;
 pub mod cleanup_policy;
 pub mod container_engine;
 pub mod git_dir_kind;
+pub mod project_description;
+pub mod project_emblem;
+pub mod project_name;
+pub mod project_version;
 pub mod remote_action_reference;
 pub mod repo_path;
 pub mod repository_name;
@@ -17,6 +21,8 @@ pub use self::{
     act_event::ActEvent, act_input::ActInput, act_job::ActJob, act_run_config::ActRunConfig,
     act_workflow::ActWorkflow, action_reference::ActionReference, cleanup_policy::CleanupPolicy,
     container_engine::ContainerEngine, git_dir_kind::GitDirKind,
+    project_description::ProjectDescription, project_emblem::ProjectEmblem,
+    project_name::ProjectName, project_version::ProjectVersion,
     remote_action_reference::RemoteActionReference, repo_path::RepoPath,
     repository_name::RepositoryName, secret::Secret, shell_command::ShellCommand,
 };
