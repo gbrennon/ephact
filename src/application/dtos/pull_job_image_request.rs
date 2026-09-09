e31f4724139128e5b34e235b419370df3d63147d
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:43:43 -0300

    refactor(application): update pull job image request

--- src/application/dtos/pull_job_image_request.rs
+++ src/application/dtos/pull_job_image_request.rs
@@ -3,7 +3,7 @@
 /// inbound port.
 pub struct PullJobImageRequest<'a> {
     /// Runner label the job declared, when it declared one.
-    runs_on: Option<&'a str>,
+    pub runs_on: Option<&'a str>,
 }
 
 impl<'a> PullJobImageRequest<'a> {
