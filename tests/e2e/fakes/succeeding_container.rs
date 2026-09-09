c5b026670c4477e1d82bcfc52dbdf9d00bd357ed
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:47:11 -0300

    test(e2e): update succeeding container

--- tests/e2e/fakes/succeeding_container.rs
+++ tests/e2e/fakes/succeeding_container.rs
@@ -28,11 +28,7 @@ impl ContainerPort for SucceedingContainer {
         env: &HashMap<String, String>,
     ) -> Result<ExecResult, ContainerError> {
         self.activity.record_command(cmd, env);
-        Ok(ExecResult {
-            exit_code: 0,
-            stdout: String::new(),
-            stderr: String::new(),
-        })
+        Ok(ExecResult::new(0, String::new(), String::new()))
     }
 
     fn copy_to(&self, container_path: &str, _entries: &[FileEntry]) -> Result<(), ContainerError> {
