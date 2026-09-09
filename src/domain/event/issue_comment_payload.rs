6f6d11ec4a6506bf200235ef9dd70e410cac22fa
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:44:31 -0300

    refactor(domain): update issue comment payload

--- src/domain/event/issue_comment_payload.rs
+++ src/domain/event/issue_comment_payload.rs
@@ -16,8 +16,20 @@ pub struct IssueCommentPayload {
 }
 
 impl IssueCommentPayload {
-    pub fn new(action: String, issue: IssueInfo, comment: CommentInfo, repository: RepositoryInfo, sender: UserInfo) -> Self {
-        Self { action, issue, comment, repository, sender }
+    pub fn new(
+        action: String,
+        issue: IssueInfo,
+        comment: CommentInfo,
+        repository: RepositoryInfo,
+        sender: UserInfo,
+    ) -> Self {
+        Self {
+            action,
+            issue,
+            comment,
+            repository,
+            sender,
+        }
     }
 
     pub fn action(&self) -> &str {
@@ -38,5 +50,45 @@ impl IssueCommentPayload {
 
     pub fn sender(&self) -> &UserInfo {
         &self.sender
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn repository() -> RepositoryInfo {
+        RepositoryInfo::new(
+            "repo".into(),
+            "owner/repo".into(),
+            UserInfo::new("name".into(), "email".into(), "login".into()),
+            false,
+            "html".into(),
+            "main".into(),
+            "clone".into(),
+            "ssh".into(),
+        )
+    }
+
+    #[test]
+    fn new_preserves_fields() {
+        let user = UserInfo::new("name".into(), "email".into(), "login".into());
+        let issue = IssueInfo::new(
+            1,
+            "title".into(),
+            None,
+            "open".into(),
+            user.clone(),
+            Vec::new(),
+            "url".into(),
+        );
+        let comment = CommentInfo::new(2, "body".into(), user.clone(), "comment".into());
+        let payload =
+            IssueCommentPayload::new("created".into(), issue, comment, repository(), user);
+
+        assert_eq!(payload.action(), "created");
+        assert_eq!(payload.issue().number(), 1);
+        assert_eq!(payload.comment().id(), 2);
+        assert_eq!(payload.repository().name(), "repo");
+        assert_eq!(payload.sender().login(), "login");
     }
 }
