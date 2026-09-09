d9984f1a8c3b98a337cb8c8addb8e8d47a5e99d2
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:46:27 -0300

    test(common): update fake runtime

--- tests/common/fakes/fake_runtime.rs
+++ tests/common/fakes/fake_runtime.rs
@@ -86,10 +86,6 @@ impl ContainerRuntimePort for FakeRuntime {
     }
 
     fn get_host_info(&self) -> Result<HostInfo, ContainerError> {
-        Ok(HostInfo {
-            os: "linux".into(),
-            arch: "amd64".into(),
-            engine_version: "1.0".into(),
-        })
+        Ok(HostInfo::new("linux", "amd64", "1.0"))
     }
 }
