13cebf95a379278a53400ade8726a9c4b38eeaf9
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:43:03 -0300

    refactor(application): update build job environment response

--- src/application/dtos/build_job_environment_response.rs
+++ src/application/dtos/build_job_environment_response.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 /// outbound port.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BuildJobEnvironmentResponse {
-    env: HashMap<String, String>,
+    pub env: HashMap<String, String>,
 }
 
 impl BuildJobEnvironmentResponse {
