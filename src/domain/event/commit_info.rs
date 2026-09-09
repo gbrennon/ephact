d652dee691f8a3765a8bb05f7058d7f74ac9f763
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:44:26 -0300

    refactor(domain): update commit info

--- src/domain/event/commit_info.rs
+++ src/domain/event/commit_info.rs
@@ -16,8 +16,26 @@ pub struct CommitInfo {
 }
 
 impl CommitInfo {
-    pub fn new(id: String, message: String, timestamp: String, author: UserInfo, committer: UserInfo, added: Vec<String>, removed: Vec<String>, modified: Vec<String>) -> Self {
-        Self { id, message, timestamp, author, committer, added, removed, modified }
+    pub fn new(
+        id: String,
+        message: String,
+        timestamp: String,
+        author: UserInfo,
+        committer: UserInfo,
+        added: Vec<String>,
+        removed: Vec<String>,
+        modified: Vec<String>,
+    ) -> Self {
+        Self {
+            id,
+            message,
+            timestamp,
+            author,
+            committer,
+            added,
+            removed,
+            modified,
+        }
     }
 
     pub fn id(&self) -> &str {
@@ -50,5 +68,36 @@ impl CommitInfo {
 
     pub fn modified(&self) -> &[String] {
         &self.modified
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn user() -> UserInfo {
+        UserInfo::new("name".into(), "email".into(), "login".into())
+    }
+
+    #[test]
+    fn new_preserves_fields() {
+        let commit = CommitInfo::new(
+            "id".into(),
+            "message".into(),
+            "timestamp".into(),
+            user(),
+            user(),
+            vec!["added".into()],
+            vec!["removed".into()],
+            vec!["modified".into()],
+        );
+
+        assert_eq!(commit.id(), "id");
+        assert_eq!(commit.message(), "message");
+        assert_eq!(commit.timestamp(), "timestamp");
+        assert_eq!(commit.author().login(), "login");
+        assert_eq!(commit.committer().login(), "login");
+        assert_eq!(commit.added(), &["added".to_string()]);
+        assert_eq!(commit.removed(), &["removed".to_string()]);
+        assert_eq!(commit.modified(), &["modified".to_string()]);
     }
 }
