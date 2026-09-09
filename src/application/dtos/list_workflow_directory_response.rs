9b50e1a33761a90b69af45ff08adc5bae9821116
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:43:33 -0300

    refactor(application): update list workflow directory response

--- src/application/dtos/list_workflow_directory_response.rs
+++ src/application/dtos/list_workflow_directory_response.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 #[derive(Debug)]
 pub struct ListWorkflowDirectoryResponse {
     /// Workflow files found directly inside the directory, sorted by path.
-    workflow_files: Vec<PathBuf>,
+    pub workflow_files: Vec<PathBuf>,
 }
 
 impl ListWorkflowDirectoryResponse {
