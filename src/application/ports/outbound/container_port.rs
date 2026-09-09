cbe9b1fa4fb4e0da73968d9614a5896b2a95063d
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:44:12 -0300

    refactor(application): update container port

--- src/application/ports/outbound/container_port.rs
+++ src/application/ports/outbound/container_port.rs
@@ -35,10 +35,10 @@ pub trait ContainerPort: Send + Sync {
     ) -> Result<ExecResult, ContainerError> {
         let result = self.exec(cmd, workdir, env)?;
         if !result.stdout().is_empty() {
-            on_output(OutputStream::StandardOutput, &result.stdout());
+            on_output(OutputStream::StandardOutput, result.stdout());
         }
         if !result.stderr().is_empty() {
-            on_output(OutputStream::StandardError, &result.stderr());
+            on_output(OutputStream::StandardError, result.stderr());
         }
         Ok(result)
     }
