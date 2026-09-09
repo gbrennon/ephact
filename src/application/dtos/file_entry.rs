9c00d16318396e811f49c681078ff954a9cf490f
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:43:24 -0300

    refactor(application): update file entry

--- src/application/dtos/file_entry.rs
+++ src/application/dtos/file_entry.rs
@@ -1,9 +1,9 @@
 /// A file entry for copy operations.
 #[derive(Debug, Clone)]
 pub struct FileEntry {
-    path: String,
-    content: Vec<u8>,
-    mode: u32,
+    pub path: String,
+    pub content: Vec<u8>,
+    pub mode: u32,
 }
 
 impl FileEntry {
