8eb6197c74c232126bd973a69c401f4d5f6ff532
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:43:01 -0300

    refactor(application): update build action input environment response

--- src/application/dtos/build_action_input_environment_response.rs
+++ src/application/dtos/build_action_input_environment_response.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 /// outbound port.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BuildActionInputEnvironmentResponse {
-    env: HashMap<String, String>,
+    pub env: HashMap<String, String>,
 }
 
 impl BuildActionInputEnvironmentResponse {
