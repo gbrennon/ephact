cf3e6a3af4d42c89f85a24287d37f83d5fa73834
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:43:15 -0300

    refactor(application): update exec result

--- src/application/dtos/exec_result.rs
+++ src/application/dtos/exec_result.rs
@@ -1,9 +1,9 @@
 /// Result of a container exec command.
 #[derive(Debug, Clone)]
 pub struct ExecResult {
-    exit_code: i64,
-    stdout: String,
-    stderr: String,
+    pub exit_code: i64,
+    pub stdout: String,
+    pub stderr: String,
 }
 
 impl ExecResult {
