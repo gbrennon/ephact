70dcb5cdb10f71d472e28922681f9a808243f52a
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:43:13 -0300

    refactor(application): update create job container request

--- src/application/dtos/create_job_container_request.rs
+++ src/application/dtos/create_job_container_request.rs
@@ -5,13 +5,13 @@ use std::path::Path;
 /// inbound port.
 pub struct CreateJobContainerRequest<'a> {
     /// Image the container is created from.
-    image: &'a str,
+    pub image: &'a str,
     /// Name the new container is given.
-    container_name: &'a str,
+    pub container_name: &'a str,
     /// Name older releases gave the same job's container.
-    legacy_container_name: &'a str,
+    pub legacy_container_name: &'a str,
     /// Repository directory mounted into the container as the workspace.
-    repo_path: &'a Path,
+    pub repo_path: &'a Path,
 }
 
 impl<'a> CreateJobContainerRequest<'a> {
