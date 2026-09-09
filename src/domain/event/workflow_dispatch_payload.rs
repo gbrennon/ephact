5f70e5307c258487b510580d2f019bd50f729dd0
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-09 03:44:46 -0300

    refactor(domain): update workflow dispatch payload

--- src/domain/event/workflow_dispatch_payload.rs
+++ src/domain/event/workflow_dispatch_payload.rs
@@ -15,8 +15,20 @@ pub struct WorkflowDispatchPayload {
 }
 
 impl WorkflowDispatchPayload {
-    pub fn new(inputs: HashMap<String, String>, repository: RepositoryInfo, sender: UserInfo, workflow: String, r#ref: String) -> Self {
-        Self { inputs, repository, sender, workflow, r#ref }
+    pub fn new(
+        inputs: HashMap<String, String>,
+        repository: RepositoryInfo,
+        sender: UserInfo,
+        workflow: String,
+        r#ref: String,
+    ) -> Self {
+        Self {
+            inputs,
+            repository,
+            sender,
+            workflow,
+            r#ref,
+        }
     }
 
     pub fn inputs(&self) -> &HashMap<String, String> {
@@ -37,5 +49,39 @@ impl WorkflowDispatchPayload {
 
     pub fn r#ref(&self) -> &str {
         &self.r#ref
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn repository() -> RepositoryInfo {
+        RepositoryInfo::new(
+            "repo".into(),
+            "owner/repo".into(),
+            UserInfo::new("name".into(), "email".into(), "login".into()),
+            false,
+            "html".into(),
+            "main".into(),
+            "clone".into(),
+            "ssh".into(),
+        )
+    }
+
+    #[test]
+    fn new_preserves_fields() {
+        let payload = WorkflowDispatchPayload::new(
+            HashMap::from([("input".into(), "value".into())]),
+            repository(),
+            UserInfo::new("name".into(), "email".into(), "login".into()),
+            "workflow.yml".into(),
+            "main".into(),
+        );
+
+        assert_eq!(payload.inputs()["input"], "value");
+        assert_eq!(payload.repository().name(), "repo");
+        assert_eq!(payload.sender().login(), "login");
+        assert_eq!(payload.workflow(), "workflow.yml");
+        assert_eq!(payload.r#ref(), "main");
     }
 }
