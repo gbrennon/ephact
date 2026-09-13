2530606ea665e213b31a4d82f035bfbf1691c853
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-13 20:53:52 -0300

    refactor(logging): export grouped failure stores

--- src/infrastructure/logging/mod.rs
+++ src/infrastructure/logging/mod.rs
@@ -1,3 +1,5 @@
 pub mod failure_log_handler;
-pub use failure_log_handler::{FailureLogErrorStore, FailureLogHandler, FailureLogPathStore};
+pub use failure_log_handler::{
+    FailureLogErrorStore, FailureLogHandler, FailureLogPathStore, FailureLogStores,
+};
 pub mod stderr_filter;
