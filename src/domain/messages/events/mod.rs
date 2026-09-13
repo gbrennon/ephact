cab9b09ff0da06e52700cc601b47d6ed01f32384
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-13 19:44:09 -0300

    feat(events): export run lifecycle payloads

--- src/domain/messages/events/mod.rs
+++ src/domain/messages/events/mod.rs
@@ -4,6 +4,8 @@ pub mod event;
 pub mod job_finished_payload;
 pub mod job_started_payload;
 pub mod output_stream;
+pub mod run_failed_payload;
+pub mod run_started_payload;
 pub mod step_finished_payload;
 pub mod step_output_payload;
 pub mod step_started_payload;
@@ -12,7 +14,8 @@ pub mod workflow_started_payload;
 pub use self::{
     act_run_completed_payload::ActRunCompletedPayload, domain_event::DomainEvent, event::Event,
     job_finished_payload::JobFinishedPayload, job_started_payload::JobStartedPayload,
-    output_stream::OutputStream, step_finished_payload::StepFinishedPayload,
+    output_stream::OutputStream, run_failed_payload::RunFailedPayload,
+    run_started_payload::RunStartedPayload, step_finished_payload::StepFinishedPayload,
     step_output_payload::StepOutputPayload, step_started_payload::StepStartedPayload,
     workflow_started_payload::WorkflowStartedPayload,
 };
