cc7d307939c9ebea7ed989ada692321bf54b5162
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-13 19:43:58 -0300

    feat(requests): add run metadata to workflow requests

--- src/application/dtos/requests/execute_workflow_request.rs
+++ src/application/dtos/requests/execute_workflow_request.rs
@@ -7,6 +7,8 @@ pub struct ExecuteWorkflowRequest<'a> {
     workflow_content: &'a str,
     repo_path: &'a Path,
     context: &'a EvaluationContext,
+    run_id: &'a str,
+    allow_repo_writes: bool,
 }
 
 impl<'a> ExecuteWorkflowRequest<'a> {
@@ -14,11 +16,15 @@ impl<'a> ExecuteWorkflowRequest<'a> {
         workflow_content: &'a str,
         repo_path: &'a Path,
         context: &'a EvaluationContext,
+        run_id: &'a str,
+        allow_repo_writes: bool,
     ) -> Self {
         Self {
             workflow_content,
             repo_path,
             context,
+            run_id,
+            allow_repo_writes,
         }
     }
 
@@ -32,5 +38,12 @@ impl<'a> ExecuteWorkflowRequest<'a> {
 
     pub fn context(&self) -> &'a EvaluationContext {
         self.context
+    }
+    pub fn run_id(&self) -> &'a str {
+        self.run_id
+    }
+
+    pub fn allow_repo_writes(&self) -> bool {
+        self.allow_repo_writes
     }
 }
