a71c19c32be5a21415dc9250451a1fe927b11df4
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-15 20:29:29 -0300

    refactor(steps): share execute step containers

--- src/infrastructure/steps/execute_step_factory.rs
+++ src/infrastructure/steps/execute_step_factory.rs
@@ -1,8 +1,5 @@
 use crate::application::ports::inbound::ExecuteStepPort;
 use crate::application::ports::outbound::ContainerPort;
 
-pub type ExecuteStepFactory = Box<
-    dyn for<'container> Fn(&'container dyn ContainerPort) -> Box<dyn ExecuteStepPort + 'container>
-        + Send
-        + Sync,
->;
+pub type ExecuteStepFactory =
+    Box<dyn Fn(std::sync::Arc<dyn ContainerPort>) -> Box<dyn ExecuteStepPort> + Send + Sync>;
