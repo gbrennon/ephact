b16831dd002153828f75d61a7f32ace3025d5453
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-20 15:00:54 -0300

    feat(tui): expose run configuration screen

--- src/presentation/tui/screens/mod.rs
+++ src/presentation/tui/screens/mod.rs
@@ -3,9 +3,11 @@ pub mod emblem;
 pub mod home;
 pub mod list_actions;
 pub mod list_workflows;
+pub mod run_configuration;
 pub mod run_workflow;
 pub mod splash;
 
 pub use list_actions::ListActionsScreen;
 pub use list_workflows::ListWorkflowsScreen;
+pub use run_configuration::{ConfigurationAction, RunConfigurationValues};
 pub use run_workflow::RunWorkflowScreen;
