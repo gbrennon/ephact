1b907ea541d68f30023e787c44614a7ada0e48e8
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-20 17:01:03 -0300

    refactor(tui): register screen manager

--- src/presentation/tui/screens/mod.rs
+++ src/presentation/tui/screens/mod.rs
@@ -5,9 +5,11 @@ pub mod list_actions;
 pub mod list_workflows;
 pub mod run_configuration;
 pub mod run_workflow;
+pub mod screen_manager;
 pub mod splash;
 
 pub use list_actions::ListActionsScreen;
 pub use list_workflows::ListWorkflowsScreen;
 pub use run_configuration::{ConfigurationAction, RunConfigurationValues};
 pub use run_workflow::RunWorkflowScreen;
+pub use screen_manager::ScreenManager;
